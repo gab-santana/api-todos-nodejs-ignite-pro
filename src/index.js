34cf9327539cd7b98632c39f7e9c99aab869d1f8
Gabriel Santana <gabrielsantana100@hotmail.com>
2022-12-15 00:18:38 -0300

    update middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,72 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const usernameAlreadyExists = users.find((user) => user.username === username)
+
+  if (!usernameAlreadyExists) {
+    return response.status(404).json({ error: "Username not found!" })
+  }
+
+  request.user = usernameAlreadyExists
+
+  next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+if(user.pro === false && user.todos.length === 10){
+  return response.status(403).json({error: "Update Pro"})
+}
+
+  next()
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find((user) => user.username === username)
+  
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" })
+  }
+  
+  const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi
+  const testUuid = regexExp.test(id)
+
+
+  if (testUuid === false) {
+    return response.status(400).json({ error: "Todo not found!" })
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id)
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found!" })
+  }
+
+  request.user = user
+
+  request.todo = todo
+
+  next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find((user) => user.id === id)
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" })
+  }
+
+  request.user = user
+  next()
 }
 
 app.post('/users', (request, response) => {
